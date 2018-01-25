d3481c1e4c32250c57d8687d54157ab2151292d8
Pasha <pvolchak@pax8.com>
2018-01-25 16:33:05 -0700

    changed path for config location

--- tweepy_connector/tweepy_client.py
+++ tweepy_connector/tweepy_client.py
@@ -4,7 +4,7 @@ import json
 class TwitterClient:
 
     def __init__(self):
-        config = json.load(open("config.json", 'r'))
+        config = json.load(open("resources/config.json", 'r'))
         auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
         auth.set_access_token(config["access_token"], config["access_token_secret"])
         self.client = tweepy.API(auth)
