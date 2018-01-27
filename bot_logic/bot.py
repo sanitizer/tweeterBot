762c111b26f1d84321811c4f261523308518be30
Pasha <pashka.zevs@gmail.com>
2018-01-26 21:02:36 -0700

    added some methods to retrieve data from api and post tweets

--- bot_logic/bot.py
+++ bot_logic/bot.py
@@ -6,8 +6,34 @@ class Bot:
     def __init__(self):
         self.connector = TwitterClient().connect()
 
-    def get_my_twitts(self):
+    def get_home_timeline(self):
         return self.connector.home_timeline()
 
+    def get_friend_tweets(self, userId):
+        return self.connector.user_timeline(userId)
 
-print(Bot().get_my_twitts())
+    def get_tweet_calls_stats(self):
+        stats = Stats()
+        data = self.connector.rate_limit_status()
+        stats.limit = data["resources"]["tweets"]["/tweets/search/:product/:label"]["limit"]
+        stats.remaining = data["resources"]["tweets"]["/tweets/search/:product/:label"]["remaining"]
+        return stats
+
+    def create_tweet(self, tweet):
+        return self.connector.update_status(tweet)
+
+    def get_followers(self):
+        return self.connector.followers()
+
+
+class Stats:
+
+    def __init__(self):
+        self.limit = 0
+        self.remaining = 0
+
+    def __str__(self):
+        return "limit: {} remaining: {}".format(self.limit, self.remaining)
+
+
+bot = Bot()
